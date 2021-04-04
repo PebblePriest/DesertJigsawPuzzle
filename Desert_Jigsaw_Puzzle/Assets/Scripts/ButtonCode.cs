b60af369b1535aff9e9763a0dc18a21e76cc58cd
PebblePriest <rkdsway@hotmail.com>
2021-04-04 17:45:47 -0600

    AR Started implementing
    Started with an AR plane identifier and some other stuff and got another panel in.

---
+++ Desert_Jigsaw_Puzzle/Assets/Scripts/ButtonCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject TitlePanel;
+    public GameObject LessonPanel;
+    public void StartButton()
+    {
+        TitlePanel.SetActive(false);
+        LessonPanel.SetActive(true);
+
+    }
+    public void DesertButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
