b421bb427d5eb52f2da8ae2d2b7a86bd24eb24e8
PebblePriest <rkdsway@hotmail.com>
2021-04-07 14:49:09 -0600

    Got predictions working

--- Desert_Jigsaw_Puzzle/Assets/Scripts/ButtonCode.cs
+++ Desert_Jigsaw_Puzzle/Assets/Scripts/ButtonCode.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class ButtonCode : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject TitlePanel;
     public GameObject LessonPanel;
+    public GameObject PredictionsPanel;
     public void StartButton()
     {
         TitlePanel.SetActive(false);
         LessonPanel.SetActive(true);
-
+        PredictionsPanel.SetActive(false);
     }
     public void DesertButton()
     {
+        LessonPanel.SetActive(false);
+        PredictionsPanel.SetActive(true);
+        TitlePanel.SetActive(false);
+    }
+    public void Continue()
+    {
         SceneManager.LoadScene(1);
     }
+   
 }
