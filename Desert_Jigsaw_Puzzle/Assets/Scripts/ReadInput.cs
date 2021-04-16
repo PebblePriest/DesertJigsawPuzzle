78d249f16413438e42dbb9fe08396106e43af9b7
PebblePriest <rkdsway@hotmail.com>
2021-04-16 16:34:02 -0600

    Added Input Field tracking and Imported Models

--- Desert_Jigsaw_Puzzle/Assets/Scripts/ReadInput.cs
+++ Desert_Jigsaw_Puzzle/Assets/Scripts/ReadInput.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadInput : MonoBehaviour
 {
     private string input;
+    public InputField Prediction1;
+    public InputField Prediction2;
+    public InputField Prediction3;
+    public InputField Reflection;
+
+    public Text p1;
+    public Text p2;
+    public Text p3;
+    public Text r1;
+    public Text showPredictions;
+
+    public GameObject NextButton;
+    public GameObject RNextButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +31,34 @@ public class ReadInput : MonoBehaviour
     {
         
     }
-    public void ReadStringInput(string s)
+    public void Prediction1Input(string s)
     {
         input = s;
         Debug.Log(s);
+        p1.text = s;
+        
     }
+    public void Prediction2Input(string s)
+    {
+        input = s;
+        Debug.Log(s);
+        p2.text = s;
+        
+    }
+    public void Prediction3Input(string s)
+    {
+        input = s;
+        Debug.Log(s);
+        p3.text = s;
+        showPredictions.text = "1. " + p1.text + "\n" + "2. " + p2.text + "\n" + "3. " + p3.text;
+        NextButton.SetActive(true);
+    }
+    public void Reflect(string s)
+    {
+        input = s;
+        Debug.Log(s);
+        r1.text = s;
+        RNextButton.SetActive(true);
+    }
+
 }
