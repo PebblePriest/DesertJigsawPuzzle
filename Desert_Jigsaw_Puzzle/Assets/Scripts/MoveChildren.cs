1ed7173380400cc27dec3fa44013e72a59b7f9b3
PebblePriest <rkdsway@hotmail.com>
2021-04-22 17:34:33 -0600

    Changed Screen Sizes

---
+++ Desert_Jigsaw_Puzzle/Assets/Scripts/MoveChildren.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChildren : MonoBehaviour
+{
+    public GameObject fullTerrain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.parent = fullTerrain.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
